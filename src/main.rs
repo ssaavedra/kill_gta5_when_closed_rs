5a1742c39cbbecc314ff1c384e46b6b58054b052
Santiago Saavedra <info@ssaavedra.eu>
2021-06-07 23:11:21 +0200

    Add conditional compilation of clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Use this to prevent the console from appearing
 // #![windows_subsystem = "windows"]
+#[cfg(feature = "clap")]
 extern crate clap;
 extern crate winapi;
 
@@ -8,19 +9,21 @@ mod raw;
 use processes::get_processes_by_name;
 use std::{thread, time};
 
+#[cfg(feature = "clap")]
 use clap::Clap;
 
 const DEFAULT_PROCESS_NAME: &str = "gta5.exe";
 
+#[cfg(feature = "clap")]
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Santiago Saavedra")]
-struct CliOpts {
+struct ClapCliOpts {
     #[clap(short, long, env, default_value = DEFAULT_PROCESS_NAME)]
     process_name: String,
 
     #[clap(short, parse(from_occurrences))]
     verbose: i32,
-    
+
     #[clap(short = 'n', long)]
     dry_run: bool,
 
@@ -28,7 +31,13 @@ struct CliOpts {
     loop_seconds: Option<u64>,
 }
 
-
+#[cfg(not(feature = "clap"))]
+struct BaseCliOpts {
+    process_name: String,
+    verbose: i32,
+    dry_run: bool,
+    loop_seconds: Option<u64>,
+}
 
 #[derive(Debug)]
 enum ProgramStatus {
@@ -51,17 +60,30 @@ impl<T> OptionFlatmap<T> for Option<T> {
     fn flat_map<U, F, I>(self, f: F) -> Option<U>
     where
         F: FnOnce(T) -> I,
-        I: IntoIterator<Item = U>
+        I: IntoIterator<Item = U>,
     {
         self.map(|item| f(item).into_iter().next()).unwrap_or(None)
     }
 }
 
-
-
-
 fn main() {
-    let opts = CliOpts::parse();
+    #[cfg(feature = "clap")]
+    let opts = ClapCliOpts::parse();
+    #[cfg(not(feature = "clap"))]
+    let opts = BaseCliOpts {
+        process_name: <String as std::str::FromStr>::from_str(DEFAULT_PROCESS_NAME)
+            .expect("infallible"),
+
+        #[cfg(debug_assertions)]
+        dry_run: true,
+        #[cfg(not(debug_assertions))]
+        dry_run: false,
+        loop_seconds: None,
+        #[cfg(debug_assertions)]
+        verbose: 1,
+        #[cfg(not(debug_assertions))]
+        verbose: 0,
+    };
 
     if opts.verbose > 0 {
         println!("Running in verbose mode.");
@@ -130,12 +152,10 @@ fn main() {
             program_status = ProgramStatus::StatusClosed;
         }
 
-        let default_seconds = if opts.verbose > 0 {
-            1
-        } else {
-            120
-        };
+        let default_seconds = if opts.verbose > 0 { 1 } else { 120 };
 
-        thread::sleep(time::Duration::from_secs(opts.loop_seconds.unwrap_or(default_seconds)));
+        thread::sleep(time::Duration::from_secs(
+            opts.loop_seconds.unwrap_or(default_seconds),
+        ));
     }
 }
